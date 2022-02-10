25202719f2f9355e42dc6f2bbdbe721cf0e5f59a
Jan Hlubuček <jan.hlubucek@bootiq.io>
2022-02-10 21:51:39 +0100

    dev

--- load/load_dev.sh
+++ load/load_dev.sh
@@ -15,7 +15,13 @@ alias check='~/scripts/check.sh'
 
 alias ccsdc='docker exec -it jhlubucek_ccs bash'
 alias ccsrestart='docker restart jhlubucek_ccs'
-alias dclist='docker ps | grep jhlub'
+alias dclist='docker ps | grep ":812[0-9]-\|jhl"'
+
 
 alias bedc='docker exec -it php-bin-extractor bash'
+alias stdc='docker exec -it switchio-token_jhlubucek_php_1 bash'
+alias dpdc='docker exec -it kafka_data_pump bash'
+alias kpdc='docker exec -it kafka_php bash'
+alias mbdc='docker exec -it monet-backend-jhlubucek_app_1 bash'
 
+alias reload='. ~/.bashrc'
